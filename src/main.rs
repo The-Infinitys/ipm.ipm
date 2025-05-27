049455c248306de11ee270fa9c4a7a12d6ce5680
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-05-27 16:06:59 +0900

    init and import depends

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+use cmd_arg::cmd_arg;
+use ipak::dprintln;
+fn main() -> Result<(), std::io::Error> {
+    let command_data = cmd_arg::get();
+    dprintln!("{}", command_data);
+    let opts = command_data.opts;
+
+    // 引数がない場合は早期リターン
+    if opts.is_empty() {
+        return Err(std::io::Error::from(std::io::ErrorKind::InvalidInput));
+    }
+
+    // let command = &opts[0];
+    // let sub_opts: Vec<&cmd_arg::Option> = opts[1..].iter().collect();
+
     println!("Hello, world!");
+    Ok(())
 }
