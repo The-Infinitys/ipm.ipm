89f39876c6775bb46eee113f91568145a348daf1
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-05-27 20:58:44 +0900

    modified

--- src/modules/messages.rs
+++ src/modules/messages.rs
@@ -1,5 +1,7 @@
 use cmd_arg::cmd_arg;
 use ipak::dprintln;
+use colored::Colorize;
+use std::env::consts::ARCH;
 pub fn help(args: Vec<&cmd_arg::Option>) -> Result<(), std::io::Error> {
     for arg in args {
         dprintln!("Arg: {}", arg);
@@ -10,5 +12,6 @@ pub fn manual() -> Result<(), std::io::Error> {
     Ok(())
 }
 pub fn version() -> Result<(), std::io::Error> {
+    println!("{} {} ({})",env!("CARGO_PKG_NAME").bold(),env!("CARGO_PKG_VERSION"),ARCH);
     Ok(())
 }
