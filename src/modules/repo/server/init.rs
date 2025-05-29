1e1593d46bf6079f0e2aca88a3068ab47bf34506
The Infinity's <the.infinity.s.infinity@gmail.com>
2025-05-29 04:36:58 +0000

    OK!

--- src/modules/repo/server/init.rs
+++ src/modules/repo/server/init.rs
@@ -1,7 +1,15 @@
 use cmd_arg::cmd_arg;
 use colored::Colorize;
-use ipak::utils::{generate_email_address, shell::username};
+use ipak::utils::files::{dir_creation, file_creation};
+use ipak::{
+    dprintln,
+    utils::{generate_email_address, shell::username},
+};
+use serde::{Deserialize, Serialize};
+use serde_yaml;
 use std::fmt;
+use std::fs;
+#[derive(Serialize, Deserialize)]
 struct ServerRepoInitOptions {
     author_name: String,
     author_email: String,
@@ -41,6 +49,40 @@ pub fn init(args: Vec<&cmd_arg::Option>) -> Result<(), std::io::Error> {
             _ => continue,
         }
     }
-    println!("{}", opts);
+    dprintln!("{}", opts);
+    server_initation(opts)?;
+    Ok(())
+}
+
+fn server_initation(
+    opts: ServerRepoInitOptions,
+) -> Result<(), std::io::Error> {
+    let entries = fs::read_dir(".")?;
+    if entries.count() > 0 {
+        eprintln!("{}", "Error: Current directory is not empty.".red());
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Current directory is not empty",
+        ));
+    }
+    struct SetUpData {
+        from: String,
+        to: String,
+    }
+    let setup_list = [SetUpData {
+        from: {
+            serde_yaml::to_string(&opts).map_err(|e| -> std::io::Error {
+                std::io::Error::new(std::io::ErrorKind::Other, e)
+            })
+        }?,
+        to: "ipm/repo.yaml".to_owned(),
+    }];
+    for setup_data in setup_list {
+        if setup_data.to.is_empty() {
+            dir_creation(&setup_data.to)?;
+        } else {
+            file_creation(&setup_data.to, &setup_data.from)?;
+        }
+    }
     Ok(())
 }
