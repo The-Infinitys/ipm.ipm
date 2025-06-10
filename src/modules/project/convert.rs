3a90afc0488eb37d44e2e71f3173ac5527bbd471
The Infinity's <the.infinity.s.infinity@gmail.com>
2025-06-10 06:19:56 +0000

    convert.rsを仮実装

--- src/modules/project/convert.rs
+++ src/modules/project/convert.rs
@@ -1,3 +1,14 @@
+use std::path::Path;
+
 pub fn convert() -> Result<(), std::io::Error> {
+    let files = ["control.tar.gz", "control.tar.xz", "control.tar.bz2", "data.tar.gz", "data.tar.xz", "data.tar.bz2", "debian-binary"];
+    for file in files.iter() {
+        if !Path::new(file).exists() {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                format!("{} not found in current directory", file),
+            ));
+        }
+    }
     Ok(())
 }
