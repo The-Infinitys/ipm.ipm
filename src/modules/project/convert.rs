5de15a9a02c2ba9b19b6f3865b6aa87f71563d4b
The Infinity's <the.infinity.s.infinity@gmail.com>
2025-06-13 15:28:36 +0900

    add convert function for debian.

--- src/modules/project/convert.rs
+++ src/modules/project/convert.rs
@@ -1,25 +1,76 @@
+use ipak::utils::archive::extract_archive;
+use std::fs;
 use std::path::Path;
 
+fn is_debian<P: AsRef<Path>>(dir: P) -> bool {
+    let dir = dir.as_ref();
+    let debian_binary = dir.join("debian-binary");
+    let control_tar = std::fs::read_dir(dir)
+        .map(|entries| {
+            entries.filter_map(|e| e.ok()).any(|e| {
+                let name = e.file_name();
+                let name = name.to_string_lossy();
+                name.starts_with("control.tar.")
+            })
+        })
+        .unwrap_or(false);
+    let data_tar = std::fs::read_dir(dir)
+        .map(|entries| {
+            entries.filter_map(|e| e.ok()).any(|e| {
+                let name = e.file_name();
+                let name = name.to_string_lossy();
+                name.starts_with("data.tar.")
+            })
+        })
+        .unwrap_or(false);
+    debian_binary.is_file() && control_tar && data_tar
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum PkgType {
+    Debian,
+    Unknown,
+}
+
+pub fn get_pkgtype<P: AsRef<Path>>(dir: P) -> PkgType {
+    if is_debian(&dir) {
+        PkgType::Debian
+    } else {
+        PkgType::Unknown
+    }
+}
+
 pub fn convert() -> Result<(), std::io::Error> {
-    let files = [
-        "control.tar.gz",
-        "control.tar.xz",
-        "control.tar.bz2",
-        "data.tar.gz",
-        "data.tar.xz",
-        "data.tar.bz2",
-        "debian-binary",
-    ];
-    for file in files.iter() {
-        if !Path::new(file).exists() {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::NotFound,
-                format!(
-                    "{} not found in current directory",
-                    file
-                ),
-            ));
+    match get_pkgtype("./") {
+        PkgType::Debian => debian(),
+        PkgType::Unknown => Err(std::io::Error::from(
+            std::io::ErrorKind::NotFound,
+        )),
+    }
+}
+
+fn debian() -> Result<(), std::io::Error> {
+    let current_dir = std::env::current_dir()?;
+
+    // 展開先ディレクトリの作成
+    let data_dir = current_dir.join("data");
+    let control_dir = current_dir.join("control");
+    fs::create_dir_all(&data_dir)?;
+    fs::create_dir_all(&control_dir)?;
+
+    // data.tar.* と control.tar.* のファイルを探す
+    let entries = fs::read_dir(&current_dir)?;
+    for entry in entries {
+        let entry = entry?;
+        let file_name = entry.file_name();
+        let file_name = file_name.to_string_lossy();
+
+        if file_name.starts_with("data.tar.") {
+            extract_archive(entry.path(), data_dir.clone())?;
+        } else if file_name.starts_with("control.tar.") {
+            extract_archive(entry.path(), control_dir.clone())?;
         }
     }
+
     Ok(())
 }
